92f5bf4c6a637eccf878adee7174a29bb1411125
崔小存 <1597828092@qq.com>
2017-09-09 09:02:52 +0800

    1

--- JingzhiForBusiness/AppDelegate.h
+++ JingzhiForBusiness/AppDelegate.h
@@ -26,14 +26,8 @@
 #endif
 // 如果需要使用idfa功能所需要引入的头文件（可选）
 #import <AdSupport/AdSupport.h>
-
-
-
 /*百度地图的*/
 #import <BaiduMapAPI_Map/BMKMapView.h>//只引入所需的单个头文件
-
-
-
 
 static NSString *appKey = @"7f98c36e1b9d6f41fe983fe7";
 static NSString *channel = @"App Store";
